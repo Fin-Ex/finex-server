a2f6bb83319d80bf52253e6127d7fa44812f1b2c
m0nster.mind <mind@m0nster.io>
2021-09-11 04:10:29 +0300

    Missing module: command module

---
+++ worldserver/src/main/java/ru/finex/ws/inject/module/loader/CommandModule.java
@@ -0,0 +1,18 @@
+package ru.finex.ws.inject.module.loader;
+
+import com.google.inject.AbstractModule;
+import ru.finex.core.inject.LoaderModule;
+import ru.finex.ws.command.InputCommandService;
+import ru.finex.ws.command.InputCommandServiceImpl;
+
+/**
+ * @author m0nster.mind
+ */
+@LoaderModule
+public class CommandModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(InputCommandService.class).to(InputCommandServiceImpl.class);
+    }
+}
