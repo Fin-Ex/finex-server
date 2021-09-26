b5758eebb103100c1cf6e1624eadb8b625779b58
m0nster.mind <mind@m0nster.io>
2021-09-26 20:03:56 +0300

    Fix entity

--- core/src/main/java/ru/finex/core/model/entity/GameObjectComponentTemplate.java
+++ core/src/main/java/ru/finex/core/model/entity/GameObjectComponentTemplate.java
@@ -5,9 +5,11 @@ import lombok.Data;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -17,8 +19,8 @@ import javax.persistence.Table;
  * @project finex-server
  * @author finfan: 13.09.2021
  */
-@Entity
 @Data
+@Entity
 @Table(name = "game_object_component_templates")
 public class GameObjectComponentTemplate implements ru.finex.core.model.entity.Entity {
 	@Id
@@ -28,7 +30,7 @@ public class GameObjectComponentTemplate implements ru.finex.core.model.entity.E
 	private int persistenceId;
 	@Column(name = "component", nullable = false)
 	private String component;
-	@Column(name = "game_object_template_id", nullable = false)
-	@ManyToOne(cascade = CascadeType.ALL)
-	private int gameObjectTemplateId;
+	@JoinColumn(name = "game_object_template_id", referencedColumnName = "id", nullable = false)
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private GameObjectTemplate gameObjectTemplate;
 }
