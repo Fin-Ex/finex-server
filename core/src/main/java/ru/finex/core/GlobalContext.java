de708ec00e97a3a674f9d1edea017a8e70371407
m0nster.mind <mind@m0nster.io>
2021-12-21 06:45:11 +0300

    Storage for global context

--- core/src/main/java/ru/finex/core/GlobalContext.java
+++ core/src/main/java/ru/finex/core/GlobalContext.java
@@ -3,6 +3,7 @@ package ru.finex.core;
 import com.google.inject.Injector;
 import org.reflections.Reflections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -11,6 +12,8 @@ import java.util.Map;
 @SuppressWarnings("checkstyle:VisibilityModifier")
 public class GlobalContext {
 
+    @SuppressWarnings("checkstyle:ConstantName")
+    public static final Map<Object, Object> context = new HashMap<>();
     public static Injector injector;
     public static Map<String, String> arguments;
     public static Reflections reflections;
