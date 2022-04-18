d770d93b3efe191886651f3b7a58b6dc975a8cf0
m0nster.mind <mind@m0nster.io>
2022-04-19 00:49:34 +0300

    Close #10 Externalize queries

--- core/src/main/java/ru/finex/core/inject/module/DbModule.java
+++ core/src/main/java/ru/finex/core/inject/module/DbModule.java
@@ -1,10 +1,12 @@
 package ru.finex.core.inject.module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
 import com.google.inject.matcher.Matchers;
 import com.google.inject.name.Names;
 import org.hibernate.Session;
 import org.hibernate.service.ServiceRegistry;
+import ru.finex.core.GlobalContext;
 import ru.finex.core.db.DbSessionService;
 import ru.finex.core.db.impl.DbSessionServiceImpl;
 import ru.finex.core.db.impl.HibernateSessionProvider;
@@ -12,10 +14,17 @@ import ru.finex.core.db.impl.ServiceRegistryProvider;
 import ru.finex.core.db.impl.TransactionalMethodInterceptor;
 import ru.finex.core.db.migration.impl.ClasspathScannerImpl;
 import ru.finex.core.db.migration.impl.DataSourceProvider;
+import ru.finex.core.repository.CrudRepository;
+import ru.finex.core.repository.DefaultCrudRepository;
+import ru.finex.core.repository.RepositoryExecutorServiceProvider;
+import ru.finex.core.repository.RepositoryProxy;
 import ru.finex.evolution.ClasspathScanner;
 import ru.finex.evolution.MigrationService;
 import ru.finex.evolution.impl.MigrationServiceImpl;
 
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Proxy;
+import java.util.concurrent.ExecutorService;
 import javax.sql.DataSource;
 import javax.transaction.Transactional;
 
@@ -33,6 +42,40 @@ public class DbModule extends AbstractModule {
         bind(ClasspathScanner.class).to(ClasspathScannerImpl.class);
         bind(MigrationService.class).to(MigrationServiceImpl.class);
         bindInterceptor(Matchers.any(), Matchers.annotatedWith(Transactional.class), new TransactionalMethodInterceptor());
+        bind(ExecutorService.class).annotatedWith(Names.named("RepositoryExecutor")).toProvider(RepositoryExecutorServiceProvider.class);
+        bindCrudRepositories();
+    }
+
+    protected void bindCrudRepositories() {
+        // provide proxy implementation for all crud interfaces
+        GlobalContext.reflections.getSubTypesOf(CrudRepository.class)
+            .stream()
+            .filter(DbModule::checkRepositoryModifiers)
+            .filter(e -> e != CrudRepository.class) // TODO m0nster.mind: ignore all repositories that doesnt have end-type of generic
+            .map(interfaceType -> new RepositoryProxy(defaultCrudRepository(interfaceType), interfaceType))
+            .forEach(proxyHandler -> bind(TypeLiteral.get(proxyHandler.getInterfaceType())).toInstance(repositoryProxy(proxyHandler)));
+    }
+
+    private static Object repositoryProxy(RepositoryProxy proxyHandler) {
+        return Proxy.newProxyInstance(
+            DbModule.class.getClassLoader(),
+            new Class[] {proxyHandler.getInterfaceType()},
+            proxyHandler
+        );
+    }
+
+    private DefaultCrudRepository defaultCrudRepository(Class<? extends CrudRepository> interfaceType) {
+        var repository = DefaultCrudRepository.builder()
+            .entityTypeFromInterface(interfaceType)
+            .build();
+
+        requestInjection(repository);
+        return repository;
+    }
+
+    private static boolean checkRepositoryModifiers(Class<?> type) {
+        int modifiers = type.getModifiers();
+        return Modifier.isInterface(modifiers) && Modifier.isPublic(modifiers);
     }
 
 }
---
+++ core/src/main/java/ru/finex/core/repository/RepositoryProxy.java
@@ -0,0 +1,126 @@
+package ru.finex.core.repository;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Session;
+import ru.finex.core.db.impl.TransactionalContext;
+import ru.finex.core.model.entity.Entity;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.util.Collection;
+import java.util.concurrent.Future;
+import javax.persistence.Query;
+
+/**
+ * CRUD repository proxy.
+ * Implements all base methods from {@link AbstractCrudRepository AbstractCrudRepository} and support
+ *  to processing custom methods with {@link ru.finex.core.repository.Query Query} or {@link NamedQuery NamedQuery} annotations.
+ *
+ * @param <E> entity type
+ * @param <ID> entity id type
+ * @author m0nster.mind
+ */
+@RequiredArgsConstructor
+public class RepositoryProxy<E extends Entity<ID>, ID extends Serializable> implements InvocationHandler {
+
+    private final DefaultCrudRepository<E, ID> repository;
+    @Getter
+    private final Class<? extends CrudRepository<E, ID>> interfaceType;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        method.setAccessible(true);
+
+        Object result;
+        try {
+            result = method.invoke(repository, args);
+        } catch (IllegalArgumentException e) {
+            result = customMethodInvoke(method, args);
+        }
+
+        return result;
+    }
+
+    private Object customMethodInvoke(Method method, Object[] args) {
+        return customMethodInvoke(method, args, method.getReturnType());
+    }
+
+    private Object customMethodInvoke(Method method, Object[] args, Class<?> returnType) {
+        if (Future.class.isAssignableFrom(returnType)) {
+            ParameterizedType typeWithGeneric = (ParameterizedType) method.getGenericReturnType();
+            Class<?> generic = (Class<?>) typeWithGeneric.getActualTypeArguments()[0];
+            return repository.asyncOperation(() -> customMethodInvoke(method, args, generic));
+        } else if (Collection.class.isAssignableFrom(returnType)) {
+            return listObject(method, args);
+        } else {
+            return singleObject(method, args);
+        }
+    }
+
+    private Object listObject(Method method, Object[] args) {
+        TransactionalContext ctx = TransactionalContext.get();
+        Session session = ctx.session();
+        try {
+            Query query = getQuery(method, session);
+            passParameters(method, query, args);
+            Object result = query.getResultList();
+            ctx.commit(session);
+            return result;
+        } catch (Exception e) {
+            ctx.rollback(session);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object singleObject(Method method, Object[] args) {
+        TransactionalContext ctx = TransactionalContext.get();
+        Session session = ctx.session();
+        try {
+            Query query = getQuery(method, session);
+            passParameters(method, query, args);
+            Object result = query.getSingleResult();
+            ctx.commit(session);
+            return result;
+        } catch (Exception e) {
+            ctx.rollback(session);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Query getQuery(Method method, Session session) {
+        var metaQuery = method.getAnnotation(ru.finex.core.repository.Query.class);
+        if (metaQuery == null) {
+            var metaNamedQuery = method.getAnnotation(NamedQuery.class);
+            if (metaNamedQuery == null || StringUtils.isBlank(metaNamedQuery.value())) {
+                return session.createNamedQuery(interfaceType.getSimpleName() + "." + method.getName());
+            }
+
+            return session.createNamedQuery(metaNamedQuery.value());
+        }
+
+        return session.createQuery(metaQuery.value());
+    }
+
+    private void passParameters(Method method, Query query, Object[] args) {
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            var param = parameters[i];
+
+            String name;
+            QueryParam metaParam = param.getAnnotation(QueryParam.class);
+            if (metaParam == null) {
+                name = param.getName();
+            } else {
+                name = metaParam.value();
+            }
+
+            query.setParameter(name, args[i]);
+        }
+    }
+
+}
