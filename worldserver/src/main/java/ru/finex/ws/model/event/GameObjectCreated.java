657a7a02719f3b21dc5484ac836584eb0c484f06
m0nster.mind <mind@m0nster.io>
2022-12-02 02:01:20 +0300

    World: fix events

--- worldserver/src/main/java/ru/finex/ws/model/event/GameObjectCreated.java
+++ worldserver/src/main/java/ru/finex/ws/model/event/GameObjectCreated.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.finex.core.model.event.GameObjectEvent;
+import ru.finex.core.pool.PooledObject;
 
 /**
  * @author m0nster.mind
  */
 @Data
+@PooledObject(maxSize = "400")
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameObjectCreated implements GameObjectEvent {
--- worldserver/src/main/java/ru/finex/ws/model/event/GameObjectDestroyed.java
+++ worldserver/src/main/java/ru/finex/ws/model/event/GameObjectDestroyed.java
@@ -1,12 +1,18 @@
 package ru.finex.ws.model.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.finex.core.model.event.GameObjectEvent;
+import ru.finex.core.pool.PooledObject;
 
 /**
  * @author m0nster.mind
  */
 @Data
+@PooledObject(maxSize = "400")
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameObjectDestroyed implements GameObjectEvent {
 
     private int runtimeId;
